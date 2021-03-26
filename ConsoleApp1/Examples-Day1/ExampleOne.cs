3808b7c90d142270cd765ab687a1d0fa8f5eb708
Ugochukwu Umeoke <ugochukwu.umeoke@decagon.dev>
2021-03-26 17:24:05 +0100

    Add project files.

---
+++ ConsoleApp1/Examples-Day1/ExampleOne.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.Examples_Day1
+{
+    class ExampleOne
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World");
+
+            Console.ReadKey();
+        }
+    }
+}
