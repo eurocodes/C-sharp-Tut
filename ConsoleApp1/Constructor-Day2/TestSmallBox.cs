6a7c49f145673eaf6c3c015b1bf13242b43969b7
Ugo Umeoke <laryhug209@yahoo.com>
2021-03-28 00:14:26 +0100

    Added encapsulation and data hiding

---
+++ ConsoleApp1/Constructor-Day2/TestSmallBox.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.Constructor_Day2
+{
+    class TestSmallBox
+    {
+        static void Main()
+        {
+            SmallBox fedEx1 = new SmallBox();
+            // fedEx1.length = 6;
+            // fedEx1.width = 5;
+            fedEx1.CalculatorArea();
+
+            SmallBox fedEx2 = new SmallBox();
+            fedEx2.length = 8;
+            fedEx2.width = 4;
+            fedEx2.CalculatorArea();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp1/Day-2-Encapsulation/TestEmployee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.Day_2_Encapsulation
+{
+    class TestEmployee
+    {
+        static void Main()
+        {
+            {
+                Employee alex = new Employee();
+                Employee linda = new Employee();
+
+                alex.SetSalary(900000);
+                // alex.bonus = 20000;
+                double alexTotalPay = alex.CalculateTotalPay();
+                Console.WriteLine("Alex Total Pay is {0}", alexTotalPay);
+
+                linda.SetSalary(100000);
+                // linda.bonus = 20000;
+                double lindaTotalPay = linda.CalculateTotalPay();
+                Console.WriteLine("Linda Total Pay is {0}", lindaTotalPay);
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
